b59a4d28c5d2d79e8ee0015b8c6294859d8077fb
cabralfbenja <cabralbenja2001@gmail.com>
2024-04-17 23:01:39 -0300

    BCF: TP6 - Primer Componente

---
+++ Material Práctico/Evaluables/MP-Evaluable-TP6-TangoApp/app/src/screens/Cotizaciones.screen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const CotizacionesScreen = () => {
+    return(
+        <div className="cotizaciones">
+            <h1>Listado de Cotizaciones de Pedido</h1>
+            
+        </div>
+    );
+}
---
+++ Material Práctico/Evaluables/MP-Evaluable-TP6-TangoApp/app/src/screens/index.js
@@ -0,0 +1,3 @@
+import {CotizacionesScreen} from "./Cotizaciones.screen.jsx";
+export { CotizacionesScreen };
+
